21e7708de30463b2800696885623135e0ffc7a37
Travis CI User <gattana@us.ibm.com>
2018-03-02 17:37:45 +0000

    bd98311 Ok going to run all of the languages in a fork i created. Also forgot natural language understanding. adding that one in.

--- visual-recognition/src/main/java/com/ibm/watson/developer_cloud/visual_recognition/v3/model/FaceAge.java
+++ visual-recognition/src/main/java/com/ibm/watson/developer_cloud/visual_recognition/v3/model/FaceAge.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 IBM Corp. All Rights Reserved.
+ * Copyright 2018 IBM Corp. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -15,8 +15,7 @@ package com.ibm.watson.developer_cloud.visual_recognition.v3.model;
 import com.ibm.watson.developer_cloud.service.model.GenericModel;
 
 /**
- * Provides age information about a face. If there are more than 10 faces in an image, the response might return the
- * confidence score `0g.
+ * Provides age information about a face. If there are more than 10 faces in an image, the response might return the confidence score `0g.
  */
 public class FaceAge extends GenericModel {
 
@@ -49,7 +48,7 @@ public class FaceAge extends GenericModel {
   /**
    * Gets the score.
    *
-   * Confidence score for the property. Scores range from 0-1, with a higher score indicating greater correlation.
+   * Confidence score for the property in the range of 0 to 1. A higher score indicates greater likelihood that the class is depicted in the image. The default threshold for returning scores from a classifier is 0.5.
    *
    * @return the score
    */
@@ -84,3 +83,4 @@ public class FaceAge extends GenericModel {
     this.score = score;
   }
 }
+
